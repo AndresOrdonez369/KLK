c5b0afe39c97d71a0130152ec722627b60f47fc8
Andres Ordoñez <oswaldordonez@unicauca.edu.co>
2020-10-08 01:28:56 -0500

    Update Styles button

--- src/screens/login/styles.js
+++ src/screens/login/styles.js
@@ -12,7 +12,7 @@ export default styles = StyleSheet.create({
     },  
     logo: {
         marginTop: height * 0.02, 
-        marginBottom: height * 0.09, 
+        marginBottom: height * 0.085, 
         height: height * 0.2,
         width: width * 0.35,
         alignItems: 'center',
@@ -24,7 +24,7 @@ export default styles = StyleSheet.create({
         marginLeft:width * 0.1,
         marginRight:width * 0.1,
         marginTop: height*-0.01,
-        marginBottom: height*0.01,
+        marginBottom: height*0.02,
         color:"#2672FF"
     },
     textLogin: {
@@ -38,6 +38,7 @@ export default styles = StyleSheet.create({
         height: height * 0.06,
         width: width * 0.8,
         borderRadius: 8,
+        marginBottom: height*0.01,
         backgroundColor: '#2672FF',
         shadowColor: 'rgba(0,0,0, .4)',
         shadowOffset: { height: 2, width: 2 },
