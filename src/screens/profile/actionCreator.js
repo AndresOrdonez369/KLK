792015e9aa9d3b4ea206d92489e119cfbedd5426
Andres Ordóñez <andrez_gato1@hotmail.com>
2020-10-14 19:22:31 -0500

    Eslint and avatar

--- src/screens/profile/actionCreator.js
+++ src/screens/profile/actionCreator.js
@@ -1,6 +1,51 @@
 import Actions from '../../redux/actionTypes';
-    
+
 export const ProfileInputUpdate = ({ prop, value }) => ({
-type: Actions.ACTUALIZARINPUT,
-payload: { prop, value },
+  type: Actions.ACTUALIZARINPUT,
+  payload: { prop, value },
+});
+
+export const userUploadImagen = (imagenURL) => async (dispatch) => {
+  try {
+    const user = firebase.auth().currentUser;
+    const userPhothoURL = `/Users/profilePhotos/${user.uid}.png`;
+    const storage = firebase.storage().ref();
+    const imagePath = storage.child(userPhothoURL);
+    const response = await fetch(imagenURL);
+    const imagenBlob = await response.blob();
+    await imagePath.put(imagenBlob);
+
+    const url = await storage
+      .child(userPhothoURL)
+      .getDownloadURL();
+
+    await user.updateProfile({
+      displayName: user.displayName,
+      photoURL: url,
+    });
+
+    return dispatch({
+      type: Actions.USER_UPDATE_IMAGEN_URL,
+      playload: url,
+    });
+  } catch (error) {
+    return dispatch({
+      type: Actions.USER_UPDATE_IMAGEN_ERROR,
+      playload: error,
+    });
+  }
+};
+
+export const showModalProfile = (message, type) => ({
+  type: Actions.SHOW_MODAL_PROFILE,
+  payload: { message, type },
+});
+
+export const hideModalProfile = () => ({
+  type: Actions.HIDE_MODAL_PROFILE,
+});
+
+export const userUpdateImagenURL = (url) => ({
+  type: Actions.USER_UPDATE_IMAGEN_URL,
+  playload: url,
 });
--- src/screens/profile/reducer.js
+++ src/screens/profile/reducer.js
@@ -1,13 +1,24 @@
 import Actions from '../../redux/actionTypes';
+
 const STATE_INICIAL = {
-    input: '',
-}
-    
-export default ( state = STATE_INICIAL, action ) => {
-    switch(action.type){
-        case Actions.ACTUALIZARINPUT:
-            return {...state, input: action.carga  };
-        default:
-            return {...state };
-    }
-}
+  input: '',
+};
+
+export default (state = STATE_INICIAL, action) => {
+  switch (action.type) {
+    case Actions.ACTUALIZARINPUT:
+      return { ...state, input: action.carga };
+    case Actions.USER_UPDATE_IMAGEN_URL:
+      return {
+        ...state,
+        imageURL: action.payload,
+      };
+    case Actions.USER_UPDATE_IMAGEN_ERROR:
+      return {
+        ...state,
+        uploadPhotoError: action.payload,
+      };
+    default:
+      return { ...state };
+  }
+};
