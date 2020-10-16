41e129433548ff8bf5075d8d522c330a0bef4ffa
Dylan <dachaconp@unal.edu.co>
2020-10-16 11:38:12 -0500

    upload Audio

--- src/screens/post/index.js
+++ src/screens/post/index.js
@@ -5,79 +5,85 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { useDispatch } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 import * as ImagePicker from 'expo-image-picker';
+import * as DocumentPicker from 'expo-document-picker';
 import BasicModal from '../../components/BasicModal';
-import { uploadAudio, uploadImage, uploadVideo, submitPost } from './actionCreator';
+import { submitPost } from './actionCreator';
 import styles from './styles';
     
 const Post = () => {
     //state
     const [body, setBody] = useState('');
     const [image, setImage] = useState(null);
-    const [showModal, setShowModal] = useState(false);
+    const [video, setVideo] = useState(null);
+    const [audio, setAudio] = useState(null);
+    const [uploaded, setUploaded] = useState(null);
     const [modal, setModal] = useState({
         modalType: 'confirmation',
+        showModal: false,
         modalTitle: '',
         modalComponent: null,
         pressOk: null,
         pressCancel: null
     });
-    const { modalTitle, modalType, modalComponent, pressCancel, pressOk } = modal;
+    const { showModal, modalTitle, modalType, modalComponent, pressCancel, pressOk } = modal;
 
     const { navigate } = useNavigation();
     const dispatch = useDispatch();
 
-    const pickImage = async () => {
+    // handle data pickers
+    const uploadStatus = (error) => {
+        setModal({
+            ...modal,
+            showModal: true,
+            modalType: error ? 'error' : 'confirmation',
+            modalTitle: error ? 'Has cancelado la carga del archivo' : 'Archivo cargado con éxito',
+            pressCancel: () => setModal({ ...modal, showModal: false })
+        });
+    }
+
+    const pickMedia = async (type) => {
         const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
         if (!granted) {
             setModal({
                 ...modal,
+                showModal: true,
                 modalType: 'error',
                 modalTitle: 'Necesitamos permisos para acceder a la galería',
-                pressCancel: () => setShowModal(false)
+                pressCancel: () => setModal({ ...modal, showModal: false })
             });
-            setShowModal(true);
             return;
         }
     
         const result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            mediaTypes: type === 'video' ? ImagePicker.MediaTypeOptions.Videos : ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
             aspect: [4, 3],
             quality: 1,
+            videoMaxDuration: 300,
         });
         if (!result.cancelled) {
-            setImage(result);
-            setModal({
-                ...modal,
-                modalType: 'interactive',
-                modalTitle: '¿Deseas seleccionar esta imagen?',
-                modalComponent: <Avatar size={160} source={image} />,
-                pressOk: () => {
-                    setImage(result);
-                    setShowModal(false);
-                },
-                pressCancel: () => {
-                    setImage(null);
-                    setShowModal(false);
-                },
-            });
-            setShowModal(true);
+            type === 'video' ? setVideo(result) : setImage(result);
+            uploadStatus(false);
+            type === 'video' ? setUploaded('video') : setUploaded('image');
         } else {
-            setModal({
-                ...modal,
-                modalType: 'error',
-                modalTitle: 'Has cancelado la carga de la imagen',
-                pressCancel: () => setShowModal(false)
-            });
-            setShowModal(true);
+            uploadStatus(true);
         }
-        dispatch(uploadImage())
     }
-    const pickVideo = () => {
-        dispatch(uploadVideo())
+    const pickAudio = async () => {
+        const audio = await DocumentPicker.getDocumentAsync({
+            type: "audio/*"
+        });
+        if (!audio.cancelled) {
+            setAudio(audio);
+            uploadStatus(false);
+            setUploaded('audio');
+        } else {
+            uploadStatus(true);
+        }
     }
-    const pickAudio = () => {
-        dispatch(uploadAudio())
+    //submit
+    const submit = (body, image, video, audio, uploaded) => {
+
     }
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: 'silver' }}>
@@ -85,9 +91,9 @@ const Post = () => {
                 <BasicModal
                     visible={showModal}
                     type={modalType}
-                    requiredHeight={0.65}
+                    requiredHeight={0.5}
                     title={modalTitle}
-                    component={modalComponent}
+                    component={modalComponent || null}
                     onPressCancel={pressCancel}
                     onPressOk={pressOk}
                 />
@@ -120,29 +126,65 @@ const Post = () => {
                             name="image"
                             type='ionicons'
                             size={30}
-                            onPress={() => pickImage()}
+                            onPress={() => uploaded === null && pickMedia('image')}
                             iconStyle={styles.icons}
                         />
                         <Icon
                             name="video"
                             type='material-community'
                             size={30}
-                            onPress={() => pickVideo()}
+                            onPress={() => uploaded === null && pickMedia('video')}
                             iconStyle={styles.icons}
                         />
                         <Icon
                             name="music-box"
                             type='material-community'
                             size={30}
-                            onPress={() => pickAudio()}
+                            onPress={() => uploaded === null && pickAudio()}
                             iconStyle={styles.icons}
                         />
                     </View>
                     <Text
                         style={styles.submitStyle}
-                        onPress={() => dispatch(submitPost(body))}
+                        onPress={() => submit(body, image, video, audio, uploaded)}
                     >Publicar</Text>
                 </View>
+                {console.log(audio)}
+                { uploaded !== null
+                    && (
+                        <View style={styles.preview}>
+                            <Icon
+                                name="close-circle"
+                                type='material-community'
+                                size={40}
+                                onPress={() => {
+                                    setImage(null);
+                                    setAudio(null);
+                                    setVideo(null);
+                                    setUploaded(null);
+                                }}
+                                iconStyle={styles.icons}
+                            />
+                            { uploaded === 'audio'
+                                ? (
+                                    <View style={{alignItems: 'center'}}>
+                                        <Icon
+                                            name="music-box"
+                                            type='material-community'
+                                            size={120}
+                                            color='#f22'
+                                            iconStyle={styles.icons}
+                                        />
+                                        <Text>{audio.name}</Text>
+                                    </View>
+                                ) : (
+                                    <Avatar
+                                        source={image || video }
+                                        size={120}
+                                    />
+                                )}
+                        </View>
+                    )}
             </View>
         </SafeAreaView>
         );
--- src/screens/post/styles.js
+++ src/screens/post/styles.js
@@ -3,70 +3,75 @@ import { Dimensions, StyleSheet } from 'react-native';
 const { height, width } = Dimensions.get('screen');
 
 export default styles = StyleSheet.create({
-    container: {
-        height,
-        width,
-        backgroundColor: 'white',
-    },
-    title: {
-        fontSize: height * 0.02,
-        color: 'black'
-    },
-    header: {
-        height: height * 0.05,
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: 'white',
-        borderBottomWidth: 1
-    },
-    buttonsContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginRight: 8 + width * 0.05,
-        marginLeft: width * 0.05
-    },
-    icon: {
-        color: '#f22',
-        marginRight: 20,
-        marginLeft: 8,
-    },
-    icons: {
-        margin: 8
-    },
-    input: {
-        paddingHorizontal: 0,
-        alignItems: 'center'
-    },
-    inputContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 8,
-        marginTop: 20
-    },
-    iconContainer: {
-        flexDirection: 'row',
-    },
-    inputStyle: {
-        justifyContent: 'flex-start',
-        height: height * 0.3,
-        width: width * 0.9,
-        borderRadius: 8,
-        backgroundColor: '#E6E6E6', // gainsboro
-        borderBottomWidth: 1,
-        borderWidth: 0.5,
-        borderColor: 'gray',
-        shadowColor: 'rgba(0,0,0, .4)',
-        shadowOffset: { height: 2, width: 2 },
-        shadowOpacity: 1,
-        shadowRadius: 2,
-        elevation: 3,
-    },
-    placeholder: {
-        alignSelf: 'flex-start'
-    },
-    submitStyle: {
-        color: '#f22',
-        fontSize: height * 0.023
-    }
+  container: {
+    height,
+    width,
+    backgroundColor: 'white',
+  },
+  title: {
+    fontSize: height * 0.02,
+    color: 'black',
+  },
+  header: {
+    height: height * 0.05,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderBottomWidth: 1,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginRight: 8 + width * 0.05,
+    marginLeft: width * 0.05,
+  },
+  icon: {
+    color: '#f22',
+    marginRight: 20,
+    marginLeft: 8,
+  },
+  icons: {
+    margin: 8,
+  },
+  input: {
+    paddingHorizontal: 0,
+    alignItems: 'center',
+  },
+  inputContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    marginTop: 20,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+  },
+  inputStyle: {
+    justifyContent: 'flex-start',
+    height: height * 0.3,
+    width: width * 0.9,
+    borderRadius: 8,
+    backgroundColor: '#E6E6E6', // gainsboro
+    borderBottomWidth: 1,
+    borderWidth: 0.5,
+    borderColor: 'gray',
+    shadowColor: 'rgba(0,0,0, .4)',
+    shadowOffset: { height: 2, width: 2 },
+    shadowOpacity: 1,
+    shadowRadius: 2,
+    elevation: 3,
+  },
+  placeholder: {
+    alignSelf: 'flex-start',
+  },
+  submitStyle: {
+    color: '#f22',
+    fontSize: height * 0.023,
+  },
+  preview: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
 });
