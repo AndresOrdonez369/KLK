d3e2ba5c0f6105125ce43ec2e5bd9b30fbcf2991
Juan Ma <jmortegao@unal.edu.co>
2020-11-16 21:33:32 -0500

    Audio fix 2.0

--- src/components/Audio/audioApp.js
+++ src/components/Audio/audioApp.js
@@ -3,6 +3,15 @@ import { Audio } from 'expo-av';
 import { audioObjUpdate } from './audioAppActionCreator';
 
 const audioApp = async () => {
+  Audio.setAudioModeAsync({
+    allowsRecordingIOS: false,
+    staysActiveInBackground: false,
+    interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+    playsInSilentModeIOS: true,
+    shouldDuckAndroid: true,
+    interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+    playThroughEarpieceAndroid: false,
+  });
   const dispatch = useDispatch();
   // const PlayerApp = new Player('https://firebasestorage.googleapis.com/v0/b/klk-messenger.appspot.com/o/posts%2Faudios%2F2%2FAnd%20It%20Was%20So.mp3?alt=media&token=a7301cb3-1bab-4ed6-883d-e18b8421bd31');
   const stateAudio = useSelector((state) => state.reducerAudio);
--- src/components/Audio/index.js
+++ src/components/Audio/index.js
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
-import { Icon, Button, } from 'react-native-elements';
-import { useDispatch, useSelector} from 'react-redux';
+import { Icon, Button } from 'react-native-elements';
+import { useDispatch, useSelector } from 'react-redux';
 import { View } from 'react-native';
-import { playerUpdate } from '../../components/Audio/audioAppActionCreator';
+import { playerUpdate } from './audioAppActionCreator';
 import styles from './styles';
 
 const AudioComponent = (mediaLink) => {
   const [audio, setAudio] = useState({
     audioController: true,
-    mediaSource: mediaLink
+    mediaSource: mediaLink,
   });
   const dispatch = useDispatch();
-  const { audioController, mediaSource} = audio;
-  const {  mediaButtonStyle, playButtonStyle, mediaContainerStyle } = styles;
-  const state = useSelector((state) => state.reducerAudio);
-  const { source } = state;
+  const { audioController, mediaSource } = audio;
+  const { mediaButtonStyle, playButtonStyle, mediaContainerStyle } = styles;
+  const audioState = useSelector((state) => state.reducerAudio);
+  const { source } = audioState;
   /* useEffect(() => {
     const createSound = async () => {
       const { sound: soundObject, status } = await Audio.Sound.createAsync(
@@ -25,7 +25,7 @@ const AudioComponent = (mediaLink) => {
   }
   createSound();
 }, []); */
-/* useEffect(() => () =>{
+  /* useEffect(() => () =>{
     async function CleanAudio() {
       console.log("entre al clean")
       await objetosonido.stopAsync();
@@ -35,11 +35,11 @@ const AudioComponent = (mediaLink) => {
     }
    return CleanAudio();
 }, []) */
-/* useEffect(() => {
+  /* useEffect(() => {
     const manejo = async () => {
       console.log("estoy en el manejo")
       await objetosonido.stopAsync()
-      setAudio({ ...audio, audioController false });           
+      setAudio({ ...audio, audioController false });
     }
     const pausaGlobal = () => globalKey == cid ? null : manejo()
     if(objetosonido != null) pausaGlobal();
@@ -50,46 +50,45 @@ const AudioComponent = (mediaLink) => {
     dispatch(playerUpdate('play'));
     console.log('aqui se monta el audio');
     setAudio({ audioController: !audioController });
-  }
+  };
   const pauseAudio = () => {
     dispatch(playerUpdate('pause'));
     setAudio({ audioController: !audioController });
-  }
+  };
   const stopAudio = () => {
     dispatch(playerUpdate('stop'));
     setAudio({ audioController: !audioController });
-  }
-  
-   const renderButtons = () => {
-    return audioController ? ( 
+  };
+
+  const renderButtons = () => (audioController ? (
     <Button
       raised
       onPress={() => playAudio()}
       buttonStyle={playButtonStyle}
       icon={
-        <Icon name={'play'} type="material-community" color="black" size={150} />
-      }
-    />) : (
-      <View style={mediaContainerStyle}>
-         <Button
-      raised
-      onPress={() => stopAudio()}
-      buttonStyle={mediaButtonStyle}
-      icon={
-        <Icon name={"stop"} type="material-community" color="black" size={120} />
+        <Icon name="play" type="material-community" color="black" size={150} />
       }
     />
-     <Button
-      raised
-      onPress={() => pauseAudio()}
-      buttonStyle={mediaButtonStyle}
-      icon={
-        <Icon name={"pause"} type="material-community" color="black" size={120} />
+  ) : (
+    <View style={mediaContainerStyle}>
+      <Button
+        raised
+        onPress={() => stopAudio()}
+        buttonStyle={mediaButtonStyle}
+        icon={
+          <Icon name="stop" type="material-community" color="black" size={120} />
       }
-    />
-      </View>
-    )
-  }
+      />
+      <Button
+        raised
+        onPress={() => pauseAudio()}
+        buttonStyle={mediaButtonStyle}
+        icon={
+          <Icon name="pause" type="material-community" color="black" size={120} />
+      }
+      />
+    </View>
+  ));
   return renderButtons();
 };
 
